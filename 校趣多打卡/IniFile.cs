58231642f3b2b051a8d5e3bf1a30f951466f8d6e
hd80606b <772711681@qq.com>
2020-10-27 15:02:18 +0800

    上传

---
+++ 校趣多打卡/IniFile.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace 校趣多打卡
+
+{
+    class IniFile
+    {
+        public string Path;
+
+        public IniFile(string path)
+        {
+            this.Path = path;
+        }
+
+        /// <summary>
+        /// 遍历INI文件的键
+        /// </summary>
+        /// <param name="SectionName">节点名称</param>
+        /// <param name="iniFilename">文件路径</param>
+        /// <returns></returns>
+        public static List<string> ReadKeys(string SectionName, string iniFilename)
+        {
+            List<string> result = new List<string>();
+            Byte[] buf = new Byte[65536];
+            uint len = GetPrivateProfileStringA(SectionName, null, null, buf, buf.Length, iniFilename);
+            int j = 0;
+            for (int i = 0; i < len; i++)
+                if (buf[i] == 0)
+                {
+                    result.Add(Encoding.Default.GetString(buf, j, i - j));
+                    j = i + 1;
+                }
+            return result;
+        }
+
+        /// <summary>
+        /// 遍历INI文件节点
+        /// </summary>
+        /// <param name="iniFilename">文件路径</param>
+        /// <returns></returns>
+        public static List<string> ReadSections(string iniFilename)
+        {
+            List<string> result = new List<string>();
+            Byte[] buf = new Byte[65536];
+            uint len = GetPrivateProfileStringA(null, null, null, buf, buf.Length, iniFilename);
+            int j = 0;
+            for (int i = 0; i < len; i++)
+                if (buf[i] == 0)
+                {
+                    result.Add(Encoding.Default.GetString(buf, j, i - j));
+                    j = i + 1;
+                }
+            return result;
+        }
+
+        /// <summary>
+        /// 写入INI文件
+        /// </summary>
+        /// <param name="section">节点名称[如[TypeName]]</param>
+        /// <param name="key">键</param>
+        /// <param name="val">值</param>
+        /// <param name="filepath">文件路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filepath);
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="section">节点名称</param>
+        /// <param name="key">键</param>
+        /// <param name="def">值</param>
+        /// <param name="retval">stringbulider对象</param>
+        /// <param name="size">字节大小</param>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retval, int size, string filePath);
+
+        /// <summary>
+        /// 读INI文件方法A
+        /// </summary>
+        /// <param name="section">节点名称</param>
+        /// <param name="key">键</param>
+        /// <param name="def">值</param>
+        /// <param name="retval">stringbuilder对象</param>
+        /// <param name="size">字节对象</param>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32")]
+        private static extern uint GetPrivateProfileStringA(string section, string key, string def, byte[] retval, int size, string filePath);
+        /// <summary>
+        /// 写入
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="iValue"></param>
+        public void WriteContentValue(string section, string key, string iValue)
+        {
+            WritePrivateProfileString(section, key, iValue, this.Path);
+        }
+
+        /// <summary>
+        /// 读取INI文件中的内容方法
+        /// </summary>
+        /// <param name="Section">键</param>
+        /// <param name="key">值</param>
+        /// <returns></returns>
+        public string ReadContentValue(string Section, string key)
+        {
+            StringBuilder temp = new StringBuilder(1024);
+            GetPrivateProfileString(Section, key, "", temp, 1024, this.Path);
+            return temp.ToString();
+        }
+    }
+}
---
+++ 校趣多打卡/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using RestSharp;
+
+namespace 校趣多打卡
+{
+    class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            IniFile newIniFile;
+            string appDataDic = Directory.GetCurrentDirectory() + @"\数据.ini";
+            string path = Directory.GetCurrentDirectory() + @"\log.txt";
+            if (!File.Exists(path))  //判断是否存在log，如不存在则创建
+            {
+                FileStream stream = System.IO.File.Create(path);
+                stream.Close();
+                stream.Dispose();
+            }
+           
+                if (!File.Exists(appDataDic))  //判断是否存在ini，如不存在则创建
+            {
+                FileStream fs = new FileStream(appDataDic, FileMode.Create);
+                fs.Flush();
+                fs.Close();
+                newIniFile = new IniFile(appDataDic);
+                newIniFile.WriteContentValue("test", "Cookie", "JSESSIONID=XXXXXXXXXXXXXXX");
+                newIniFile.WriteContentValue("test", "CheckPlace", "宇宙-银河系-地球");
+                newIniFile.WriteContentValue("test", "Temperature", "36.5");
+                newIniFile.WriteContentValue("test", "Phone", "XXXXXXXXXXX");
+                newIniFile.WriteContentValue("test", "livingPlace", "宇宙-银河系-地球");
+                newIniFile.WriteContentValue("test", "livingPlaceDetail", "亚洲");
+                newIniFile.WriteContentValue("test", "checkPlaceProvince", "宇宙");
+                newIniFile.WriteContentValue("test", "checkPlaceCity", "银河系");
+                newIniFile.WriteContentValue("test", "checkPlaceArea", "地球");
+                newIniFile.WriteContentValue("1", "Cookie", "JSESSIONID=");
+                newIniFile.WriteContentValue("1", "CheckPlace", "");
+                newIniFile.WriteContentValue("1", "Temperature", "36.4");
+                newIniFile.WriteContentValue("1", "Phone", "");
+                newIniFile.WriteContentValue("1", "livingPlace", "");
+                newIniFile.WriteContentValue("1", "livingPlaceDetail", "");
+                newIniFile.WriteContentValue("1", "checkPlaceProvince", "");
+                newIniFile.WriteContentValue("1", "checkPlaceCity", "");
+                newIniFile.WriteContentValue("1", "checkPlaceArea", "");
+            }
+            StreamWriter writer = new StreamWriter(path, true);
+            newIniFile = new IniFile(appDataDic);  //建立对象，并遍历所有节点名，不为test的运行
+            List<string> list = IniFile.ReadSections(appDataDic);
+            for (int i=1;i<list.Count;i++)
+            {
+                var client = new RestClient("https://mps.zocedu.com/corona/submitHealthCheck/submit");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Cookie", newIniFile.ReadContentValue(list[i], "Cookie"));
+                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.AddParameter("checkPlace", newIniFile.ReadContentValue(list[i], "CheckPlace"));
+                request.AddParameter("contactMethod", newIniFile.ReadContentValue(list[i], "Phone"));
+                request.AddParameter("teacher", "");
+                request.AddParameter("temperature", newIniFile.ReadContentValue(list[i], "Temperature"));
+                request.AddParameter("isCohabitFever", "否");
+                request.AddParameter("isLeavePalce", "否");
+                request.AddParameter("beenPlace", "");
+                request.AddParameter("isContactNcov", "否");
+                request.AddParameter("livingPlace", newIniFile.ReadContentValue(list[i], "livingPlace"));
+                request.AddParameter("livingPlaceDetail", newIniFile.ReadContentValue(list[i], "livingPlaceDetail"));
+                request.AddParameter("name1", "");
+                request.AddParameter("relation1", "");
+                request.AddParameter("phone1", "");
+                request.AddParameter("name2", "");
+                request.AddParameter("relation2", "");
+                request.AddParameter("phone2", "");
+                request.AddParameter("remark", "");
+                request.AddParameter("extraInfo", "[]");
+                request.AddParameter("healthStatus", "z");
+                request.AddParameter("emergencyContactMethod", "[]");
+                request.AddParameter("checkPlacePoint", "124,37");
+                request.AddParameter("checkPlaceDetail", "");
+                request.AddParameter("checkPlaceCountry", "");
+                request.AddParameter("checkPlaceProvince", newIniFile.ReadContentValue(list[i], "checkPlaceProvince"));
+                request.AddParameter("checkPlaceCity", newIniFile.ReadContentValue(list[i], "checkPlaceCity"));
+                request.AddParameter("checkPlaceArea", newIniFile.ReadContentValue(list[i], "checkPlaceArea"));
+                IRestResponse response = client.Execute(request);
+                if (response.Content == "")
+                {
+                    Console.WriteLine("已成功打卡\n");
+                    writer.WriteLine(DateTime.Now + "\0" + list[i] + "已成功打卡\n");
+                    writer.Flush();
+                    writer.Close();
+                }
+                else
+                {
+                    Console.WriteLine(response.Content + "\n打卡失败，请检查cookie是否失效\n");
+                    writer.WriteLine(DateTime.Now + "\0"+ list[i] + "打卡失败，请检查cookie是否失效\n");
+                    writer.Flush();
+                    writer.Close();
+                    //Console.WriteLine(list[i]);
+                }
+            }
+            Thread.Sleep(3000);
+        }
+    }
+}
